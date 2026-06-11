479b93a24953932a812bcc1467716609374ce1c2
Wiktoria Drabińska <wiktoria.drabinska@gmail.com>
2026-06-11 20:47:29 +0200

    Made size of map responsive

--- data_processor.py
+++ data_processor.py
@@ -1,5 +1,6 @@
 import json
 import requests
+from sklearn.cluster import KMeans
 
 response = requests.get("https://www.poznan.pl/mim/plan/map_service.html?mtype=pub_transport&co=cluster")
 
@@ -11,6 +12,12 @@ for object in objects["features"]:
     if object["properties"]["route_type"]=="0" and object["properties"]["stop_name"] not in unique_names:
         tram_stops.append(object)
         unique_names.add(object["properties"]["stop_name"])
+
+## Sector assignment
+all_coords = [object["geometry"]["coordinates"] for object in tram_stops]
+kmeans = KMeans(10).fit(all_coords)
+for stop in tram_stops:
+    stop["sector"] = int(kmeans.predict([stop["geometry"]["coordinates"]])[0])
 
 with open("./mini-tram/src/assets/stops.json", "w", encoding="utf-8") as json_file:
     json.dump(tram_stops, json_file)
--- mini-tram/src/App.jsx
+++ mini-tram/src/App.jsx
@@ -3,9 +3,12 @@ import all_stops from "./assets/stops.json"
 import { scaleLat, scaleLon, selectStops, colors } from "./utils.js" 
 
 function Stop({props}) {
-  return <div className="stop" style={{
-    position: "absolute", top: `${900*scaleLat(props.geometry.coordinates[1])}px`, left: `${900*scaleLon(props.geometry.coordinates[0])}px`, color: colors[props.sector]
-  }}>
+  let style = {
+    "--lat" : scaleLat(props.geometry.coordinates[1]),
+    "--lon" : scaleLon(props.geometry.coordinates[0]),
+    color: colors[props.sector]
+  }
+  return <div className="stop" style={style}>
   <div>O</div>
   <div className="stop-description">{props.properties.stop_name}</div>
   </div>
