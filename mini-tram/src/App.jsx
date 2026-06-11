eda6b719280e3b4155460649a29b825b35b75a7a
Wiktoria Drabińska <wiktoria.drabinska@gmail.com>
2026-06-11 21:20:59 +0200

    Added selecting stops

--- mini-tram/src/App.jsx
+++ mini-tram/src/App.jsx
@@ -2,28 +2,43 @@ import { useState } from 'react'
 import all_stops from "./assets/stops.json"
 import { scaleLat, scaleLon, selectStops, colors } from "./utils.js" 
 
-function Stop({props}) {
+function Stop({props, selected, onClick}) {
   let style = {
     "--lat" : scaleLat(props.geometry.coordinates[1]),
     "--lon" : scaleLon(props.geometry.coordinates[0]),
-    color: colors[props.sector]
+    color : selected? "RebeccaPurple": "black"
   }
-  return <div className="stop" style={style}>
+  console.log(props)
+  return <div className="stop" style={style} onClick={onClick}>
   <div>O</div>
   <div className="stop-description">{props.properties.stop_name}</div>
   </div>
 }
 
+
 function App() {
   const [stops, setStops] = useState(all_stops)
   const [routes, setRoutes] = useState([])
-  console.log(stops)
+  const [selected, setSelected] = useState([])
+
+  
+  function handleClick(stop) {
+    let current_selected = selected.slice()
+    if (selected.includes(stop.id)) {
+      current_selected = selected.filter(id => id != stop.id)
+    } else {
+      current_selected.push(stop.id)
+    }
+    setSelected(current_selected)
+  }
 
   return (
-    <div className="map">
-      {stops.map((stop) => {
-        return <Stop key={stop.stop_id} props={stop}></Stop>
-      })}
+    <div className="app">
+      <div className="map">
+        {stops.map((stop) => {
+          return <Stop key={stop.id} props={stop} selected={selected.includes(stop.id)} onClick={() => handleClick(stop)}></Stop>
+        })}
+      </div>
     </div>
   )
 }
