ae3004552bb60074aae096be87d20402c7e55109
Wiktoria Drabińska <wiktoria.drabinska@gmail.com>
2026-06-11 19:15:00 +0200

    Added sectors

--- mini-tram/src/App.jsx
+++ mini-tram/src/App.jsx
@@ -1,22 +1,10 @@
 import { useState } from 'react'
 import all_stops from "./assets/stops.json"
-
-let min_lat = 37.53//Math.min(...new Set(all_stops.map((stop) => 90-stop.geometry.coordinates[1])))
-let max_lat = 37.64//Math.max(...new Set(all_stops.map((stop) => 90-stop.geometry.coordinates[1])))
-let min_lon = 16.81//Math.min(...new Set(all_stops.map((stop) => stop.geometry.coordinates[0])))
-let max_lon = 17.03//Math.max(...new Set(all_stops.map((stop) => stop.geometry.coordinates[0])))
-
-
-function scaleLat(lat) {
-  return (90-lat - min_lat)/(max_lat - min_lat)
-}
-function scaleLon(lon) {
-  return (lon - min_lon)/(max_lon - min_lon)
-}
+import { scaleLat, scaleLon, selectStops, colors } from "./utils.js" 
 
 function Stop({props}) {
   return <div className="stop" style={{
-    position: "absolute", top: `${900*scaleLat(props.geometry.coordinates[1])}px`, left: `${900*scaleLon(props.geometry.coordinates[0])}px`
+    position: "absolute", top: `${900*scaleLat(props.geometry.coordinates[1])}px`, left: `${900*scaleLon(props.geometry.coordinates[0])}px`, color: colors[props.sector]
   }}>
   <div>O</div>
   <div className="stop-description">{props.properties.stop_name}</div>
@@ -25,6 +13,8 @@ function Stop({props}) {
 
 function App() {
   const [stops, setStops] = useState(all_stops)
+  const [routes, setRoutes] = useState([])
+  console.log(stops)
 
   return (
     <div className="map">
---
+++ mini-tram/src/utils.js
@@ -0,0 +1,30 @@
+let min_lat = 37.53//Math.min(...new Set(all_stops.map((stop) => 90-stop.geometry.coordinates[1])))
+let max_lat = 37.64//Math.max(...new Set(all_stops.map((stop) => 90-stop.geometry.coordinates[1])))
+let min_lon = 16.81//Math.min(...new Set(all_stops.map((stop) => stop.geometry.coordinates[0])))
+let max_lon = 17.03//Math.max(...new Set(all_stops.map((stop) => stop.geometry.coordinates[0])))
+export const colors = ["BlueViolet", "Coral", "CornflowerBlue", "PaleGreen", "Orange", "RebeccaPurple", "Tomato", "DarkSalmon", "Indigo", "Crimson"]
+
+Array.prototype.random = function () {
+  return this[Math.floor((Math.random()*this.length))];
+}
+
+export function scaleLat(lat) {
+  return (90-lat - min_lat)/(max_lat - min_lat)
+}
+
+export function scaleLon(lon) {
+  return (lon - min_lon)/(max_lon - min_lon)
+}
+
+export function selectStop(all_stops, current_stops) {
+  let available_stops = all_stops.filter(stop => !current_stops.includes(stop))
+  return available_stops.random()
+}
+
+export function selectStops(n, all_stops, current_stops) {
+    let selected_stops = []
+    for (let i = 0; i < n; ++i) {
+        selected_stops.push(selectStop(all_stops, current_stops.concat(selected_stops)))
+    }
+    return selected_stops
+}
